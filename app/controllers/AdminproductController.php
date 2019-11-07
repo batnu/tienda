469890807a6d908cbd097c81276b3b50c5fdce77
batnu <batnu_piro@hotmail.com>
2019-11-07 18:32:46 +0000

    crud productos2.3

--- app/controllers/AdminproductController.php
+++ app/controllers/AdminproductController.php
@@ -33,9 +33,28 @@ class AdminproductController extends Controller
 	}
 	public function create()
 	{
+		$errors = [];
+		$dataForm = [];
 		$type = $this->model->getConfig('productType');
 		$status = $this->model->getConfig('productStatus');
 		$catalogue = $this->model->getCatalogue();
+
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+			//Recibimps la informacion del formulario
+			//$type = isset($_POST['type']) ? $_POST['type'] : '';
+			//Este es equivalente a la de arriba 
+			$type = isset($_POST['type']) ?? '';
+
+			//validamos la informacion 
+			//Creamos el array con los datos
+
+			if (empty($errors)) {
+				//enviamos datos al modelo
+				if(empty($errors)){
+					//Redirigimos al index de adminproduct
+				}
+			}
+		}
 
 		$data = [
 			'title' => 'Administración de productos - Alta',
@@ -43,7 +62,9 @@ class AdminproductController extends Controller
 			'admin' => true,
 			'type' => $type,
 			'status' => $status,
-			'catalogue' => $catalogue
+			'catalogue' => $catalogue,
+			'errors' => $errors,
+			'data' => $dataForm
 		];
 		$this->view('admin/products/create',$data);
 
