947e8c39f9ed393705a041b3ef271b5f7534c2d7
batnu <batnu_piro@hotmail.com>
2019-11-07 16:49:59 +0000

    crud productos2.1

--- app/views/admin/products/create.php
+++ app/views/admin/products/create.php
@@ -1,4 +1,5 @@
 <?php include_once (VIEWS . 'header.php') ?>
+<script src="<?= ROOT ?>js/admincreateproduct.js"></script>
 <div class="card p-4 bg-light">
 	<div class="card-header">
 		<h1 class="text-center">Alta de un producto</h1>
@@ -56,6 +57,59 @@
 				<label for="price">Precio del producto:</label>
 				<input type="text" name="price" class="form-control" placeholder="Escribe el precio sin comas ni símbolos" value="<?= isset($data['data']['price']) ? $data['data']['price'] : ''?>">
 			</div>
+
+			<div class="form-group text-left">
+				<label for="descount">Descuento del producto:</label>
+				<input type="text" name="discount" class="form-control" placeholder="Escribe el descuento del producto sin comas ni símbolos" value="<?= isset($data['data']['discount']) ? $data['data']['discount'] : ''?>">
+			</div>
+
+			<div class="form-group text-left">
+				<label for="send">Coste del envío del producto:</label>
+				<input type="text" name="send" class="form-control" placeholder="Escribe el coste del envío del producto sin comas ni símbolos" value="<?= isset($data['data']['send']) ? $data['data']['send'] : ''?>">
+			</div>
+			<div class="form-group text-left">
+				<label for="image">Imágen del producto</label>
+				<input type="file" name="image" class="form-control">
+			</div>
+			<div class="form-group text-left">
+				<label for="published">Fecha de publicación del producto</label>
+				<input type="date" name="published" class="form-control" placeholder="Fecha de creación o publicación del producto (AAAA-MM-DD)" value="<?= isset($data['data']['published']) ? $data['data']['published'] : ''?>">
+			</div>
+			<div class="form-group text-left">
+				<label for="relation1">Producto relacionado:</label>
+				<select name="relation1" id="relation1" class="form-control">
+					<option value="">Selecciona un producto relacionado:</option>
+				</select>
+			</div>
+			<div class="form-group text-left">
+				<label for="relation2">Producto relacionado:</label>
+				<select name="relation2" id="relation2" class="form-control">
+					<option value="">Selecciona un producto relacionado:</option>
+				</select>
+			</div>
+			<div class="form-group text-left">
+				<label for="relation3">Producto relacionado:</label>
+				<select name="relation3" id="relation3" class="form-control">
+					<option value="">Selecciona un producto relacionado:</option>
+				</select>
+			</div>
+			<div class="form-group text-left">
+				<label for="status">Estado del producto</label>
+				<select name="status" id="status" class="form-control">
+					<option value="">Selecciona el estado del producto:</option>
+				</select>
+			</div>
+			<div class="form-group text-left">
+				<label for="mostSold">
+					<input type="checkbox" name="mostSold" id="mostSold">Producto más vendido
+				</label>
+			</div>			
+			<div class="form-group text-left">
+				<label for="new">
+					<input type="checkbox" name="new" id="new">Producto nuevo
+				</label>
+			</div>
+
 			<div class="form-group text-left">
 				<input type="submit" value="Enviar" class="btn btn-success">
 				<a href="<?= ROOT ?>adminproduct" class="btn btn-info">Cancelar</a>
